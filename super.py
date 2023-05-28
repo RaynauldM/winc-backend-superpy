545b96b16b270884e6e1362773da50eff514e7a4
RaynauldM <raynauldminkema@gmail.com>
2023-05-28 11:18:10 +0200

    -added rich module -added mock data -removed test, to be reinstated next session

--- super.py
+++ super.py
@@ -3,6 +3,10 @@ import argparse
 import csv
 from datetime import date
 
+# my own imports
+
+from rich import print
+
 # Do not change these lines.
 __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
 __human_name__ = "superpy"
@@ -10,7 +14,7 @@ __human_name__ = "superpy"
 
 # Your code below this line.
 def main():
-    
+    pass
 
 
 if __name__ == "__main__":
